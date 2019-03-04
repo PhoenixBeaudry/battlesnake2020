450fe42f18eb2f9eb4411fc1164455b80ebd7407
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2019-03-04 19:37:14 +0000

    tupleToGraphIndex

--- graphfunctions.jl
+++ graphfunctions.jl
@@ -3,7 +3,9 @@
 #Takes the x, y value in a Tuple and converts it
 #to the required index in the MetaGraph
 #TODO Dont assume square graph
-function tupleToGraphIndex(tuple, boardsize):
-	return 1
+function tupleToGraphIndex(tuple, boardsize)
+	x = tuple.x + 1
+	y = tuple.y
+	return (x + y*boardsize)
 end
 
