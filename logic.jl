e29a4395d9ce485f09652e482b01c8f7368bff5c
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2019-03-03 23:12:26 -0800

    SimpleGraph Type inside GameState Struct

--- logic.jl
+++ logic.jl
@@ -1,8 +1,11 @@
-import MetaGraphs
+using LightGraphs, MetaGraphs
 
 mutable struct GameState
 	game
 	turn
 	board
 	you
+	graph::Union{Nothing, SimpleGraph}
+	GameState(game, turn, board, you) = new(game, turn, board, you)
+	GameState(oldgamestate, graph) = new(oldgamestate.game, oldgamestate.turn, oldgamestate.board, oldgamestate.you, graph)
 end
--- server.jl
+++ server.jl
@@ -4,20 +4,24 @@ import HTTP
 import JSON2
 import Sockets
 
+
 #respondToPing
 #When POST is sent to /ping endpoint returns HTTP 200
 function respondToPing(req::HTTP.Request)
 	return HTTP.Response(200)
 end
 
+
 #respondToStart
 #When POST is sent to /move responds with Snake move
 function respondToStart(req::HTTP.Request)
 	#Retrieve Initial Board State and construct the GameState
 	currentGameState = JSON2.read(IOBuffer(HTTP.payload(req)), GameState)
+	currentGameState = GameState(currentGameState, LightGraphs.SimpleGraphs.Grid([currentGameState.board.width, currentGameState.board.height]))
 	println(currentGameState)
 	return HTTP.Response(200)
 end
+
 
 #respondToMove
 #When POST is sent to /move responds with Snake move
@@ -28,11 +32,15 @@ function respondToMove(req::HTTP.Request)
 end
 
 
+
+
+
 #Define our endpoints and their corresponding functions
 const SNAKE_ROUTER = HTTP.Router()
+HTTP.@register(SNAKE_ROUTER, "POST", "/start", respondToStart)
 HTTP.@register(SNAKE_ROUTER, "POST", "/move", respondToMove)
 HTTP.@register(SNAKE_ROUTER, "POST", "/ping", respondToPing)
-HTTP.@register(SNAKE_ROUTER, "POST", "/start", respondToStart)
+
 
 
 #Start the HTTP Server
